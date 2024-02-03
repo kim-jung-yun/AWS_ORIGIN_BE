ae77510eed3614b48f45e0143afc327c53baae2c
kim-jung-yun <jungyun5535@gmail.com>
2024-02-03 15:24:30 +0900

    swagger

--- src/main/java/com/ssgtarbucks/SwaggerConfig.java
+++ src/main/java/com/ssgtarbucks/SwaggerConfig.java
@@ -2,8 +2,10 @@ package com.ssgtarbucks;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
 
+import com.fasterxml.classmate.TypeResolver;
 
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
@@ -11,30 +13,27 @@ import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
-//Swagger2
-//http://localhost:8000/swagger-ui.html
+@EnableWebMvc
 @Configuration
-@EnableSwagger2
-public class SwaggerConfig implements WebMvcConfigurer {
-	private ApiInfo apiInfo() {
-
-        return new ApiInfoBuilder()
-                .title("Demo")
-                .description("API EXAMPLE")
+public class SwaggerConfig extends WebMvcConfigurationSupport  {
+	
+	@Bean
+    public Docket api(TypeResolver typeResolver) {
+        return new Docket(DocumentationType.OAS_30) // 3.0 문서버전으로 세팅
+                .useDefaultResponseMessages(true)
+                .apiInfo(apiInfo())
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("com.ssgtarbucks.controller"))
+                .paths(PathSelectors.any())
                 .build();
     }
 
-    @Bean
-    public Docket commonApi() {
-        return new Docket(DocumentationType.SWAGGER_2)
-                .groupName("ssgtarbucks")
-                .apiInfo(this.apiInfo())
-                .select()
-                .apis(RequestHandlerSelectors
-                        .basePackage("com.ssgtarbucks.controller"))
-                .paths(PathSelectors.ant("/**"))
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+                .title("Swagger 3.0 Api Sample")
+                .description("This is Sample")
+                .version("1.0")
                 .build();
     }
 }
